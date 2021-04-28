83487433981dfed3c771f9ce97e4f779cf72aee2
ajay14sharma <ajay14sharma@gmail.com>
2021-04-28 19:10:23 +0000

    First Commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into TODO
+values(10001, 'First desc', false, sysdate(), 'ajaykums');
+insert into TODO
+values(10002, 'Second desc', false, sysdate(), 'ajaykums');
+insert into TODO
+values(10003, 'Third desc', false, sysdate(), 'ajaykums');
